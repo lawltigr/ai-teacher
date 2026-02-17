f2087102b96103f7931d0e834262271307342e60
lawltigr <vasilisatikhonova110@gmail.com>
2026-02-16 21:55:09 -0800

    updateeee

--- ai_teacher/teacher/views.py
+++ ai_teacher/teacher/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import TeachingStyle
+
+def select_style(request):
+    styles = TeachingStyle.objects.all()
+    return render(request, 'select_style.html', {'styles': styles})
 
 # Create your views here.
